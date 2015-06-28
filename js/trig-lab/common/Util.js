bb77dc8bea0731e03027aad3df39f9883acde693
dubson <dubson21356@gmail.com>
2015-06-28 14:23:16 -0600

    experimenting with background colors in trig-lab

--- js/trig-lab/common/Util.js
+++ js/trig-lab/common/Util.js
@@ -12,8 +12,8 @@ define( function( require ) {
     return {
         // layout bounds used throughout the simulation for laying out the screens
         LAYOUT_BOUNDS: new Bounds2( 0, 0, 768, 464 ),
-        BACKGROUND_COLOR: '#FEC', //'#FFECB3',  //'#fff', //
-        VIEW_BACKGROUND_COLOR: '#EFE',
+        BACKGROUND_COLOR: '#FFECB3',  //'#EFE', //'#fff', //
+        VIEW_BACKGROUND_COLOR: '#EFE', //'#FFD',//'#FEC',
         TEXT_COLOR: '#000',
         LINE_COLOR: '#000',
         PANEL_COLOR: '#EEE', //'#FFD9B3',  //
--- js/trig-lab/view/TrigLabScreenView.js
+++ js/trig-lab/view/TrigLabScreenView.js
@@ -44,7 +44,7 @@ define( function ( require ) {
         readoutDisplay.top = 30;
         graphView.x = this.layoutBounds.centerX;
         graphView.y = this.layoutBounds.bottom - graphView.axesNode.bottom - 15;
-        controlPanel.right = this.layoutBounds.right - 30;
+        controlPanel.right = this.layoutBounds.right - 60;
         controlPanel.top = this.layoutBounds.top + 30;
         //console.log( 'layoutBounds = '+this.layoutBounds );
 

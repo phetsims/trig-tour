5eb05d62e9cce25c8b5bd77d9de2aaeb644b930f
dubson <michael.dubson@colorado.edu>
2015-08-13 12:18:22 -0600

    minor code clean-up

--- js/trig-tour/common/Util.js
+++ js/trig-tour/common/Util.js
@@ -13,14 +13,14 @@ define( function( require ) {
     return {
         // layout bounds used throughout the simulation for laying out the screens
         LAYOUT_BOUNDS: new Bounds2( 0, 0, 768, 464 ),
-        BACKGROUND_COLOR: '#fff6cc', //'#fffee3',//'#99ff66', //'#FFFF99', //'#FFECB3',  //'#EFE', //
-        VIEW_BACKGROUND_COLOR: '#FFF', //'#EFE', //'#FFD',//'#FEC',
+        BACKGROUND_COLOR: '#fff6cc',
+        VIEW_BACKGROUND_COLOR: '#FFF',
         TEXT_COLOR: '#000',
         TEXT_COLOR_GRAY: '#AAA',
         LINE_COLOR: '#000',
-        PANEL_COLOR: '#f9f9f9',//'#EEE', //'#FFD9B3',  //
-        SIN_COLOR: '#008700',  //color-blind green //'#0C0',
+        PANEL_COLOR: '#f9f9f9',
+        SIN_COLOR: '#008700',  //color-blind green
         COS_COLOR: '#00D',     //normal blue
-        TAN_COLOR: '#ff5500'   //color-blind red//'#F00'
+        TAN_COLOR: '#ff5500'   //color-blind red
     };
 } );

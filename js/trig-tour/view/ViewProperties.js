3f2112181667b8568df1c522044d977c4e1783e1
Jesse <jesse.greenberg@colorado.edu>
2015-09-23 12:00:55 -0600

    Remove reset function in ViewProperties, see #30

--- js/trig-tour/view/ViewProperties.js
+++ js/trig-tour/view/ViewProperties.js
@@ -4,34 +4,26 @@
  */
 
 define( function( require ) {
-    'use strict';
+  'use strict';
 
-    // modules
-    var inherit = require( 'PHET_CORE/inherit' );
-    var PropertySet = require( 'AXON/PropertySet' );
+  // modules
+  var inherit = require( 'PHET_CORE/inherit' );
+  var PropertySet = require( 'AXON/PropertySet' );
 
-    /**
-     * @constructor
-     */
-    function ViewProperties() {
-        this.viewProperties = this;
+  /**
+   * @constructor
+   */
+  function ViewProperties() {
+    this.viewProperties = this;
 
-        PropertySet.call( this, {
-            graph: 'cos',          //{string} which graph is visible, 'cos'|'sin' |'tan'
-            angleUnits: 'degrees', //{string} which angle units, 'degrees'|'radians'
-            labelsVisible: false,
-            gridVisible: false,
-            specialAnglesVisible: false
-        } );
-    }
-
-    return inherit( PropertySet, ViewProperties, {
-        reset: function() {
-            this.graph = 'cos';
-            this.angleUnits = 'degrees';
-            this.labelsVisible = false;
-            this.gridVisible = false;
-            this.specialAnglesVisible = false;
-        }
+    PropertySet.call( this, {
+      graph: 'cos',          //{string} which graph is visible, 'cos'|'sin' |'tan'
+      angleUnits: 'degrees', //{string} which angle units, 'degrees'|'radians'
+      labelsVisible: false,
+      gridVisible: false,
+      specialAnglesVisible: false
     } );
+  }
+
+  return inherit( PropertySet, ViewProperties );
 } );

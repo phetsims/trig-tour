bbf954b370d10936a694b40adbbe8652bcc96828
samreid <reids@colorado.edu>
2018-11-19 12:55:11 -0700

    Update copyright dates, see https://github.com/phetsims/chipper/issues/711

--- js/trig-tour/view/ControlPanel.js
+++ js/trig-tour/view/ControlPanel.js
@@ -1,4 +1,4 @@
-// Copyright 2015-2017, University of Colorado Boulder
+// Copyright 2015-2018, University of Colorado Boulder
 
 /**
  * Control Panel for Trig Tour sim, on right side of screenView
--- js/trig-tour/view/UnitCircleView.js
+++ js/trig-tour/view/UnitCircleView.js
@@ -1,4 +1,4 @@
-// Copyright 2015-2017, University of Colorado Boulder
+// Copyright 2015-2018, University of Colorado Boulder
 
 /**
  * Unit Circle View. Has a grabbable radial arm called a rotor.

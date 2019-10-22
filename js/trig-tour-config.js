5ebedbc439734f96d409a2603b18dde20ef6e672
zepumph <michael.kauzmann@colorado.edu>
2019-10-22 13:39:16 -0800

    add UTTERANCE_QUEUE to configs, move tests over to utterance-queue repo, convert usages to use the library, not the scenery-phet ones, delete those, https://github.com/phetsims/utterance-queue/issues/2

--- js/trig-tour-config.js
+++ js/trig-tour-config.js
@@ -40,7 +40,8 @@ require.config( {
     SUN: '../../sun/js',
     TAMBO: '../../tambo/js',
     TANDEM: '../../tandem/js',
-    TRIG_TOUR: '.'
+    TRIG_TOUR: '.',
+    UTTERANCE_QUEUE: '../../utterance-queue/js'
   },
 
   // Cache busting is applied by default, but can be disabled via ?cacheBust=false, see initialize-globals.js

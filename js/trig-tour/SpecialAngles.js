c273a0ba0a1e15d721cbf0a4a46f3dd93290530e
phet-dev <phetadmin@gmail.com>
2020-01-22 04:01:20 -0700

    update copyright dates from daily grunt work

--- js/trig-tour/SpecialAngles.js
+++ js/trig-tour/SpecialAngles.js
@@ -1,4 +1,4 @@
-// Copyright 2015-2019, University of Colorado Boulder
+// Copyright 2015-2020, University of Colorado Boulder
 
 /**
  * Special angles use throughout trig-tour.  The special angles can be represented in radians and degrees.  The first

1cf0a1f83cdb0bb49ef3fd86ccb887258276ec4d
phet-dev <phetadmin@gmail.com>
2020-02-28 03:54:27 -0700

    update copyright dates from daily grunt work

--- js/trig-tour/view/TrigPlotsNode.js
+++ js/trig-tour/view/TrigPlotsNode.js
@@ -1,4 +1,4 @@
-// Copyright 2015-2019, University of Colorado Boulder
+// Copyright 2015-2020, University of Colorado Boulder
 
 /**
  * Handles drawing the shapes and creating the paths for the trig plots on the graph of Trig Tour.

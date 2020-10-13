e8fe9252619209a9990fd4a116459b876d3e8345
pixelzoom <cmalley@pixelzoom.com>
2020-10-13 10:50:34 -0600

    convert to ArrowNode and its subclasses to ES6 class, https://github.com/phetsims/tasks/issues/1044

--- js/trig-tour/view/TrigIndicatorArrowNode.js
+++ js/trig-tour/view/TrigIndicatorArrowNode.js
@@ -1,7 +1,8 @@
 // Copyright 2015-2020, University of Colorado Boulder
 
 /**
- * Vertical or horizontal arrow, consisting of a line and a triangular arrow-head.
+ * Vertical or horizontal arrow, consisting of a line and a triangular arrow-head. The head dynamically resizes to a
+ * fraction of the tail length.
  *
  * @author Michael Dubson (PhET developer) on 6/16/2015.
  * @author Jesse Greenberg
@@ -9,65 +10,64 @@
 
 import Bounds2 from '../../../../dot/js/Bounds2.js';
 import Utils from '../../../../dot/js/Utils.js';
-import inherit from '../../../../phet-core/js/inherit.js';
 import merge from '../../../../phet-core/js/merge.js';
 import ArrowNode from '../../../../scenery-phet/js/ArrowNode.js';
 import trigTour from '../../trigTour.js';
 
-/**
- * Constructor for TrigIndicatorArrowNode which is an arrow node with a head that dynamically resizes to a fraction
- * of the tail length.
- *
- * @param {number} defaultLength - arrow in view coordinates
- * @param {string} orientation -  one of 'vertical' or 'horizontal'
- * @param {Object} [options] - passed to ArrowNode
- * @constructor
- */
-function TrigIndicatorArrowNode( defaultLength, orientation, options ) {
-
-  let tipX = 0;
-  let tipY = 0;
-  this.orientation = orientation; // @private, 'horizontal' or 'vertical' orientation
-
-  if ( orientation === 'vertical' ) {
-    tipY = defaultLength;
-  }
-  else {
-    tipX = defaultLength;
-  }
-
-  ArrowNode.call( this, 0, 0, tipX, tipY, merge( {
-    isHeadDynamic: true,
-    fractionalHeadHeight: 3 / 5,
-    headHeight: 25,
-    headWidth: 15,
-    lineWidth: 0
-  }, options ) );
-
-  const emptyBounds = new Bounds2( 0, 0, 0, 0 );
-  this.computeShapeBounds = function() {
-    return emptyBounds;
-  };
-}
-
-trigTour.register( 'TrigIndicatorArrowNode', TrigIndicatorArrowNode );
-
-inherit( ArrowNode, TrigIndicatorArrowNode, {
+class TrigIndicatorArrowNode extends ArrowNode {
 
   /**
-   * @public Set the color of this arrow, with no stroke this is just the fill.
+   * Constructor for TrigIndicatorArrowNode which is an arrow node with a
+   *
+   * @param {number} defaultLength - arrow in view coordinates
+   * @param {string} orientation -  one of 'vertical' or 'horizontal'
+   * @param {Object} [options] - passed to ArrowNode
+   */
+  constructor( defaultLength, orientation, options ) {
+
+    let tipX = 0;
+    let tipY = 0;
+
+    if ( orientation === 'vertical' ) {
+      tipY = defaultLength;
+    }
+    else {
+      tipX = defaultLength;
+    }
+
+    super( 0, 0, tipX, tipY, merge( {
+      isHeadDynamic: true,
+      fractionalHeadHeight: 3 / 5,
+      headHeight: 25,
+      headWidth: 15,
+      lineWidth: 0
+    }, options ) );
+
+    this.orientation = orientation; // @private, 'horizontal' or 'vertical' orientation
+
+    const emptyBounds = new Bounds2( 0, 0, 0, 0 );
+    this.computeShapeBounds = function() {
+      return emptyBounds;
+    };
+  }
+
+  /**
+   * Sets the color of this arrow, with no stroke this is just the fill.
+   * @public
    * @param {string | Color} color
    */
-  setColor: function( color ) {
+  setColor( color ) {
     this.fill = color;
     this.stroke = color;
-  },
+  }
 
   /**
-   * Set the endpoint for the arrow.  Dependent on sign and magnitude of end point displacement.
+   * Sets the endpoint for the arrow.  Dependent on sign and magnitude of end point displacement.
    * @param displacement
+   * @public
    */
-  setEndPoint: function( displacement ) {
+  setEndPoint( displacement ) {
+
     // determine the sign of displacement if displacement is non zero
     // sign is positive if arrow is pointing up or right, negative if pointing down or left
     const sign = displacement === 0 ? 0 : Utils.roundSymmetric( displacement / Math.abs( displacement ) );
@@ -76,11 +76,11 @@ inherit( ArrowNode, TrigIndicatorArrowNode, {
     if ( this.orientation === 'vertical' ) {
       this.setTailAndTip( this.tailX, this.tailY, this.tipX, -sign * arrowLength );
     }
-    else if ( this.orientation === 'horizontal' ) {
+    else {
       this.setTailAndTip( this.tailX, this.tailY, sign * arrowLength, this.tipY );
     }
   }
+}
 
-} );
-
+trigTour.register( 'TrigIndicatorArrowNode', TrigIndicatorArrowNode );
 export default TrigIndicatorArrowNode;

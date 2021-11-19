ef2c17eef82ba1b31f1d6c964768bc09ae1ed5a1
Sam Reid <reids@colorado.edu>
2021-11-18 17:44:08 -0700

    Run grunt update, see https://github.com/phetsims/chipper/issues/1148

---
+++ js/trigTourStrings.ts
@@ -0,0 +1,36 @@
+// Copyright 2020-2021, University of Colorado Boulder
+
+/**
+ * Auto-generated from modulify, DO NOT manually modify.
+ */
+/* eslint-disable */
+import getStringModule from '../../chipper/js/getStringModule.js';
+import trigTour from './trigTour.js';
+
+type StringsType = {
+  'trig-tour': {
+    'title': string
+  },
+  'angle': string,
+  'cos': string,
+  'degrees': string,
+  'grid': string,
+  'labels': string,
+  'rads': string,
+  'radians': string,
+  'sin': string,
+  'specialAngles': string,
+  'tan': string,
+  'x': string,
+  'y': string,
+  'values': string,
+  'vs': string,
+  'valueUnitPattern': string,
+  'numberPiPattern': string
+};
+
+const trigTourStrings = getStringModule( 'TRIG_TOUR' ) as StringsType;
+
+trigTour.register( 'trigTourStrings', trigTourStrings );
+
+export default trigTourStrings;

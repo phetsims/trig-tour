5d04ff39bb19345787f153bba6f6bf94bc30ad11
phet-dev <phetadmin@gmail.com>
2022-07-21 05:47:10 -0600

    update copyright dates from daily grunt work

--- js/trig-tour/view/TrigTourScreenView.js
+++ js/trig-tour/view/TrigTourScreenView.js
@@ -1,4 +1,4 @@
-// Copyright 2015-2021, University of Colorado Boulder
+// Copyright 2015-2022, University of Colorado Boulder
 
 /**
  * Main screen view, master layout of view on stage
--- js/trig-tour/view/readout/ReadoutNode.js
+++ js/trig-tour/view/readout/ReadoutNode.js
@@ -1,4 +1,4 @@
-// Copyright 2015-2021, University of Colorado Boulder
+// Copyright 2015-2022, University of Colorado Boulder
 
 /**
  * Live readout of angle, and values of sin, cos, tan.

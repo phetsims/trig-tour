9a50631d96d0754f3f49fd34994c56ff45ccb7d5
Jonathan Olson <jonathan.olson@colorado.edu>
2022-08-15 20:36:41 -0600

    Grunt update, and changes for string Properties, see https://github.com/phetsims/chipper/issues/1302

--- js/trigTourStrings.ts
+++ js/trigTourStrings.ts
@@ -5,28 +5,46 @@
  */
 /* eslint-disable */
 import getStringModule from '../../chipper/js/getStringModule.js';
+import TReadOnlyProperty from '../../axon/js/TReadOnlyProperty.js';
 import trigTour from './trigTour.js';
 
 type StringsType = {
   'trig-tour': {
     'title': string;
+    'titleProperty': TReadOnlyProperty<string>;
   };
   'angle': string;
+  'angleProperty': TReadOnlyProperty<string>;
   'cos': string;
+  'cosProperty': TReadOnlyProperty<string>;
   'degrees': string;
+  'degreesProperty': TReadOnlyProperty<string>;
   'grid': string;
+  'gridProperty': TReadOnlyProperty<string>;
   'labels': string;
+  'labelsProperty': TReadOnlyProperty<string>;
   'rads': string;
+  'radsProperty': TReadOnlyProperty<string>;
   'radians': string;
+  'radiansProperty': TReadOnlyProperty<string>;
   'sin': string;
+  'sinProperty': TReadOnlyProperty<string>;
   'specialAngles': string;
+  'specialAnglesProperty': TReadOnlyProperty<string>;
   'tan': string;
+  'tanProperty': TReadOnlyProperty<string>;
   'x': string;
+  'xProperty': TReadOnlyProperty<string>;
   'y': string;
+  'yProperty': TReadOnlyProperty<string>;
   'values': string;
+  'valuesProperty': TReadOnlyProperty<string>;
   'vs': string;
+  'vsProperty': TReadOnlyProperty<string>;
   'valueUnitPattern': string;
+  'valueUnitPatternProperty': TReadOnlyProperty<string>;
   'numberPiPattern': string;
+  'numberPiPatternProperty': TReadOnlyProperty<string>;
 };
 
 const trigTourStrings = getStringModule( 'TRIG_TOUR' ) as StringsType;

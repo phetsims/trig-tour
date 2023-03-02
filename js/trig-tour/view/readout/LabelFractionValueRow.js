73865d374b28ec68d5370f340f8bc6ab466779f5
phet-dev <phetadmin@gmail.com>
2023-03-02 05:06:21 -0700

    update copyright dates from daily grunt work

--- js/trig-tour/view/readout/LabelFractionValueRow.js
+++ js/trig-tour/view/readout/LabelFractionValueRow.js
@@ -1,4 +1,4 @@
-// Copyright 2016-2022, University of Colorado Boulder
+// Copyright 2016-2023, University of Colorado Boulder
 
 /**
  * Creates the third row for the ReadoutNode of Trig Tour.  This row contains a label for the trig function,

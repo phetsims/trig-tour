bfe30d1ca03138ac6d05c12356430947955ce81c
Michael Kauzmann <michael.kauzmann@colorado.edu>
2024-09-26 14:10:24 -0600

    typo, https://github.com/phetsims/chipper/issues/1474
    Signed-off-by: Michael Kauzmann <michael.kauzmann@colorado.edu>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,7 @@
 // Copyright 2024, University of Colorado Boulder
 
 /**
- * ESlint configuration for trig-tour.
+ * ESLint configuration for trig-tour.
  *
  * @author Sam Reid (PhET Interactive Simulations)
  * @author Michael Kauzmann (PhET Interactive Simulations)

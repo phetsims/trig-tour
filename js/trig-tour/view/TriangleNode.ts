eb5eb168bc360f665c46f993fa238ecc6871e70f
Jesse <jesse.greenberg@colorado.edu>
2024-12-03 16:14:22 -0500

    ts conversion, see #199

--- js/trig-tour/view/TriangleNode.ts
+++ js/trig-tour/view/TriangleNode.ts
@@ -12,7 +12,7 @@
 
 import Utils from '../../../../dot/js/Utils.js';
 import { Shape } from '../../../../kite/js/imports.js';
-import { Node, Path } from '../../../../scenery/js/imports.js';
+import { Node, Path, TPaint } from '../../../../scenery/js/imports.js';
 import trigTour from '../../trigTour.js';
 
 class TriangleNode extends Node {
@@ -21,12 +21,12 @@ class TriangleNode extends Node {
    * Constructor for TriangleNode, which draws a simple triangle with center of base at (0, 0)
    * Used as arrow heads on ends of curves in GraphView
    *
-   * @param {number} length - length triangle in view coordinates
-   * @param {number} width - width of triangle in view coordinates
-   * @param {string|Color} color - string, e.g. '#0F0'
-   * @param {number} rotationInDegrees - rotation of node about (0,0)
+   * @param length - length triangle in view coordinates
+   * @param width - width of triangle in view coordinates
+   * @param color
+   * @param rotationInDegrees - rotation of node about (0,0)
    */
-  constructor( length, width, color, rotationInDegrees ) {
+  public constructor( length: number, width: number, color: TPaint, rotationInDegrees: number ) {
 
     // Call the super constructor
     super();

f357369c1566a16a466e8e3830c90605e7a33aea
phet-dev <phetadmin@gmail.com>
2025-01-08 03:50:19 -0700

    update copyright dates from daily grunt work

--- js/trig-tour/view/GraphView.ts
+++ js/trig-tour/view/GraphView.ts
@@ -1,4 +1,4 @@
-// Copyright 2015-2024, University of Colorado Boulder
+// Copyright 2015-2025, University of Colorado Boulder
 
 /**
  * View of Graph of sin, cos, or tan vs. theta, at bottom of stage, below unit circle

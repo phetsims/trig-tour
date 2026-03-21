98f35757be195812cff251c4b176c56e962ec77a
phet-dev <phetadmin@gmail.com>
2026-03-21 05:00:53 -0600

    update copyright dates from daily grunt work

--- js/trig-tour/view/AngleSoundGenerator.ts
+++ js/trig-tour/view/AngleSoundGenerator.ts
@@ -1,4 +1,4 @@
-// Copyright 2024, University of Colorado Boulder
+// Copyright 2024-2026, University of Colorado Boulder
 
 /**
  * AngleSoundGenerator is a sound generator specifically designed to produce sounds for the
--- js/trig-tour/view/TrigTourDescriber.ts
+++ js/trig-tour/view/TrigTourDescriber.ts
@@ -1,4 +1,4 @@
-// Copyright 2025, University of Colorado Boulder
+// Copyright 2025-2026, University of Colorado Boulder
 
 /**
  * Implements descriptions about Trig Tour for screen readers and Voicing.
